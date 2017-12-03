12b6750ac651f8c8f23d35566cfcc2ea971829c4
MjSeven <yuchenmj@163.com>
2017-12-03 23:10:19 +0800

    quicksort

---
+++ code/quicksort.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#coding:utf-8
+
+def quicksort(array):
+    if len(array) < 2:
+        return array
+
+    pivot = array[0]
+
+    less = [i for i in array[1:] if i < pivot]
+    great = [i for i in array[1:] if i > pivot]
+
+    return quicksort(less) + [pivot] + quicksort(great)
+
+a = [10,2,89,3,56,34,23,67,23,1,345,3623462,23,154,43]
+print quicksort(a)
+
+

db31d0d9724e7b1b2b73feadd3486becd835a8ad
MjSeven <33125422+MjSeven@users.noreply.github.com>
2018-02-24 15:16:40 +0800

    Create insert_sort.py

---
+++ code/insert_sort.py
@@ -0,0 +1,13 @@
+# 插入排序
+def insert_sort(lists):
+    # 直接插入
+    count = len(lists)
+    for i in range(1, count):
+        key = lists[i]
+        j = i - 1
+        while j >= 0:
+            if lists[j] > key:
+                lists[j + 1] = lists[j]
+                lists[j] = key
+            j -= 1
+    return lists

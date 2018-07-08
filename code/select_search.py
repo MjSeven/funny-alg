9731273cd79036ee2012eadf0cdf6ee3c7ab5b84
MjSeven <33125422+MjSeven@users.noreply.github.com>
2018-07-08 21:50:54 +0800

    Update select_search.py

--- code/select_search.py
+++ code/select_search.py
@@ -17,14 +17,21 @@ def selectionSort(arr):
     for i in xrange(len(arr)):
         smallest = findsmallest(arr)
         newarr.append(arr.pop(smallest))
-
     return newarr
 
 # 另一种写法
-def selection_sort(list_test):
+def select_sort(list_test):
     for i in range(0, len(list_test)):
         min_d = i       # 0 1
-        for j in range(i + 1, len(list_test)):      # 1 - len(list_test)
+        for j in range(i + 1, len(list_test)):      # 1 ~ len(list_test)
             if list_test[j] < list_test[min_d]:
                 min_d = j
+        # 一轮过后选出最小的一个数放在第一个位置
         list_test[i], list_test[min_d] = list_test[min_d], list_test[i]
+        
+def main():
+    alist= [54,26,93,17,77,31,44,55,20]
+    print(select_sort(alist))
+
+if __name__ == '__main__':
+    main()

e120ed4b8d40e6abc2210934209c11141db0898c
MjSeven <33125422+MjSeven@users.noreply.github.com>
2018-07-08 21:42:59 +0800

    Update bubble_sort.py

--- code/bubble_sort.py
+++ code/bubble_sort.py
@@ -1,4 +1,14 @@
-for i in range(len(L) - 1):
-    for j in range(len(L) - 1 - i):
-        if L[j] > L[j + 1]:
-            L[j], L[j + 1] = L[j + 1], L[j]
+#! /usr/binenv python
+
+def bubble_sort(L):
+    for i in range(len(L) - 1):
+        for j in range(len(L) - 1 - i):
+            if L[j] > L[j + 1]:
+                L[j], L[j + 1] = L[j + 1], L[j]
+
+def main():
+    l = [54,26,93,17,77,31,44,55,20]
+    print(bubble_sort())
+    
+if __name__ == '__main__':
+    main()

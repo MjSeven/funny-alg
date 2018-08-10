0c9b88251d9489d25fbe40c3c6b775b970ba726c
MjSeven <yuchenmj@163.com>
2018-08-10 21:51:31 +0800

    反转链表

--- swordOffer/反转链表.py
+++ swordOffer/反转链表.py
@@ -1,2 +1,23 @@
 # !/usr/bin/python3
-# @Time : 2018/8/10 21:35
+"""
+单链表反转问题
+"""
+class ListNode:
+    def __init__(self, value, next):
+        self.val = value
+        self.next = next
+
+
+def ReverseListNode(pHead):
+    pNode = pHead
+    pPrev, pReverse = None, None
+    while pNode:
+        pNext = pNode.next
+        if not pNext:
+            pReverse = pNode
+
+        pNode.next = pPrev
+        pPrev = pNode
+        pNode = pNext
+
+    return pReverse

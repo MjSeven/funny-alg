830fba6d87333c86cfa6567b17f0a828093eafcc
MjSeven <yuchenmj@163.com>
2018-08-11 15:56:35 +0800

    包含 min 函数的栈.py

--- swordOffer/包含 min 函数的栈.py
+++ swordOffer/包含 min 函数的栈.py
@@ -1,2 +1,32 @@
 # !/usr/bin/python3
-# @Time : 2018/8/11 9:36
+
+"""
+实现栈的数据结构，包含 min 函数，使得 min，push，pop 函数时间复杂度都是 O(1)
+"""
+
+
+class Stack:
+    def __init__(self):
+        self.m_data = []
+        self.m_min = []
+
+    def push(self,value):
+        self.m_data.append(value)
+        if len(self.m_min) == 0 or value < self.m_min[-1]:
+            self.m_min.append(value)
+        else:
+            self.m_min.append(self.m_min[-1])
+
+    def pop(self):
+        if self.m_data and self.m_min:
+            self.m_data.pop()
+            self.m_min.pop()
+
+    def top(self):
+        if self.m_data:
+            return self.m_data[-1]
+
+
+    def min(self):
+        if self.m_min:
+            return self.m_min[-1]

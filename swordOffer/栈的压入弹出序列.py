68771d9202e85af4c37206358675d2b1000ee1c0
MjSeven <yuchenmj@163.com>
2018-08-11 15:56:55 +0800

    栈的压入弹出序列

--- swordOffer/栈的压入弹出序列.py
+++ swordOffer/栈的压入弹出序列.py
@@ -1,2 +1,21 @@
 # !/usr/bin/python3
-# @Time : 2018/8/11 9:49
+"""
+两个整数序列，一个表示栈的压入顺序，另一个表示栈的弹出序列，判断弹出序列是否正确
+"""
+
+from collections import UserList
+
+def isPopOrder(pushV, popV):
+    stack = []
+    while popV:
+        if pushV and popV[0] == pushV[0]:
+            popV.pop(0)
+            pushV.pop(0)
+        elif stack and stack[-1] == popV[0]:
+            stack.pop()
+            popV.pop(0)
+        elif pushV:
+            stack.append(pushV.pop(0))
+        else:
+            return False
+    return True

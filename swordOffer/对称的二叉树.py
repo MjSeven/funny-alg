9ab3ea890603add1a983a20e33cd9cc0342f0f97
MjSeven <yuchenmj@163.com>
2018-08-11 15:55:58 +0800

    对称的二叉树

--- swordOffer/对称的二叉树.py
+++ swordOffer/对称的二叉树.py
@@ -1,2 +1,26 @@
 # !/usr/bin/python3
-# @Time : 2018/8/11 9:20
+"""
+判断一颗二叉树是不是对称的
+如果和它的镜像一样，则其是对称的，否则不是
+"""
+
+class BinaryTreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def isSymmetrical(root):
+    def isSame(p1, p2):
+        if not p1 and not p2:
+            return True
+        if not p1 or not p2:
+            return False
+        if p1.val != p2.val:
+            return False
+
+        return isSame(p1.left, p2.right) and isSame(p1.right, p2.left)
+
+    return isSame(root, root)
+
